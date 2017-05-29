368cb383a947c4b75f2e36f457fd4084fc07bf5d
Daniel Feuerriegel <Daniel.feuerriegel@gmail.com>
2017-05-29 10:11:05 +1000

    Removed entries of channel_names_file and channellocs from decoding configuration script, as they are not used by this set of functions

--- EXAMPLE_run_decoding_analyses.m
+++ EXAMPLE_run_decoding_analyses.m
@@ -66,8 +66,6 @@ data_struct_name = 'eeg_sorted_cond'; % Data arrays for use with DDTBOX must use
 %% EEG dataset information
 
 nchannels = 64; % number of channels
-channel_names_file = 'channel_inf.mat'; % Name of .mat file containing channel labels and channel locations
-channellocs = [bdir, 'locations/']; % Path of directory containing channel information file
 sampling_rate = 1000; % Data sampling rate in Hz
 pointzero = 100; % Corresponds to the time of the event/trigger code relative to the prestimulus baseline (in ms)
 
